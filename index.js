ea15b938d7d264907849e497bf0aa92b6eff52e3
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-06-08 12:39:13 +0300

    I added the required functionality into index.js.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const homeLabel = document.getElementById("home-label");
 const guestLabel = document.getElementById("guest-label");
+const scoreBtn = document.querySelector(".score-btn");
 
+let homecount = 0
+let guestCount = 0
+homeLabel.innerText = `${homecount}`;
+guestLabel.innerText = `${guestCount}`;
+
+function incrementHomeCountByOne() {
+  homecount += 1;
+  homeLabel.innerText = `${homecount}`;
+}
+
+function incrementHomeCountByTwo() {
+  homecount += 2;
+  homeLabel.innerText = `${homecount}`;
+}
+
+function incrementHomeCountByThree() {
+  homecount += 3;
+  homeLabel.innerText = `${homecount}`;
+}
+
+function incrementGuestCountByOne() {
+  guestCount += 1;
+  guestLabel.innerText = `${guestCount}`;
+}
+
+function incrementGuestCountByTwo() {
+  guestCount += 2;
+  guestLabel.innerText = `${guestCount}`;
+}
+
+function incrementGuestCountByThree() {
+  guestCount += 3;
+  guestLabel.innerText = `${guestCount}`;
+}
